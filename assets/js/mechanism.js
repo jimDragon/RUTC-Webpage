ab2671b2af309b0fe05a2a850846686aac4bb3c2
jimDragon <jim.dragons@gmail.com>
2020-03-20 11:56:51 +0200

    created-landing-page

---
+++ assets/js/mechanism.js
@@ -0,0 +1,58 @@
+var menu = document.getElementById('menu');
+var nav = document.getElementById('nav');
+var exit = document.getElementById('exit');
+
+// Open Menu
+menu.addEventListener('click', function(e) {
+    nav.classList.toggle('hide-mobile');
+    e.preventDefault();
+})
+
+// Close Menu
+exit.addEventListener('click', function(e) {
+    nav.classList.add('hide-mobile');
+    e.preventDefault();
+})
+
+// Navigation Smooth Scrolling
+$(document).ready(function(){
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            nav.classList.add('hide-mobile');   // Close the Menu on click
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The number (500) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 500, function(){
+        
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+            });
+        }
+    });
+});
+
+// // Back to top Button
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $('#top-scroll').fadeIn();
+        }else{
+            $('#top-scroll').fadeOut();
+        }
+    });
+    $('#top-scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
